e932c787d572bfaee783a7f145cda58748834356
Sik <contact@sikelio.wtf>
2023-12-12 16:31:54 +0100

    Added test for getting /login page

--- tests/functional/hello_world.spec.ts
+++ tests/functional/hello_world.spec.ts
@@ -1,8 +1,7 @@
-import { test } from '@japa/runner'
+import { test } from '@japa/runner';
 
-test('display welcome page', async ({ client }) => {
-  const response = await client.get('/')
+test('Display login page', async ({ client }) => {
+  const response = await client.get('/login');
 
-  response.assertStatus(200)
-  response.assertTextIncludes('<h1 class="title"> It Works! </h1>')
-})
+  response.assertStatus(200);
+});
